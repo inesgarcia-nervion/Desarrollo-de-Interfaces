3e1b6f532355b2ea5eee55bd3ecd9b83f730ca49
inesgarcia-nervion <ines.garcia@iesnervion.es>
2025-09-30 13:47:19 +0200

    Tercera subida

--- Aplicacion1/app/index.tsx
+++ Aplicacion1/app/index.tsx
@@ -1,8 +1,9 @@
-<<<<<<< HEAD
-import { Text, View } from "react-native";
-=======
 import { Text, View, Button, Alert } from "react-native";
->>>>>>> 4bbc121316ae23b3809230a4c188e8825c1a6d1b
+
+function pulsar() {
+  alert("Botón pulsado")
+}
+
 
 export default function Index() {
   return (
@@ -13,12 +14,12 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-<<<<<<< HEAD
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-=======
-    <Text>Edit app/index.tsx to edit this screen.</Text>
+    <Text>Hola mundo!</Text>
     <Button title="Inicio" onPress={() => Alert.alert('¡Hola Mundo!')} />
->>>>>>> 4bbc121316ae23b3809230a4c188e8825c1a6d1b
     </View>
   );
 }
+
+
+//También se puede hacer declarando una función y después poner: <Button title="Inicio" onPress={pulsar}/>
+
