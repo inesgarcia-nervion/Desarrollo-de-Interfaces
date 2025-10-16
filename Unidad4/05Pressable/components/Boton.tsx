909b4d03e2bda9047eb0025cb311d042f16004e2
inesgarcia-nervion <ines.garcia@iesnervion.es>
2025-10-16 12:36:54 +0200

    Ejercicio 5 y 6

--- Unidad4/05Pressable/components/Boton.tsx
+++ Unidad4/05Pressable/components/Boton.tsx
@@ -4,10 +4,10 @@ import { View, Pressable, Text } from 'react-native';
 
 function Boton({ texto }: { texto: string }) {
   return (
-    <View style = {{margin: 5}}>
-          <Pressable style = {{backgroundColor: "#0867fff", padding: 10, borderRadius: 10}} >
-            <Text style={{ color: 'white', textAlign: 'center' }}>{texto}</Text>
-          </Pressable>
+    <View style={{ margin: 5 }}>
+      <Pressable style={{ backgroundColor: "#0867fff", padding: 10, borderRadius: 10 }} >
+        <Text style={{ color: 'white', textAlign: 'center' }}>{texto}</Text>
+      </Pressable>
     </View>
   );
 }
